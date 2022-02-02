6fb1f4c82a7a62ceb1154e2658ad6c343b42010b
Truman Tang <ttang8@mail.sfsu.edu>
2022-02-02 09:12:30 -0800

    Riot game question algo

---
+++ Week14Day3.js
@@ -0,0 +1,44 @@
+
+/* 
+Given to alumni by Riot games in 2021.
+*/
+
+const str1 = "b70a164c32a20c10"; // b:70 a:164 c:32 a:20 c:10
+const expected1 = "a184b70c42"; //a:184 b:70 c:42
+//70
+
+  //given an incorrectly string by combining letter count occurences
+  // return the correctly rehashed string alphabetized.
+function rehash(s) {
+
+}
+
+// HINTS: isNaN(num) , parseInt(str)
+
+
+
+
+
+   
+// BONUS  -- longest substring without repeating characters
+
+
+const str1 = "abcabcbb";
+const expected1 = 3;
+// Explanation: The answer is "abc", with the length of 3.
+
+const str2 = "bbbbb";
+const expected2 = 1;
+// Explanation: The answer is "b", with the length of 1.
+
+const str3 = "pwwkew";
+const expected3 = 3;
+/* Explanation: The answer is "wke", with the length of 3. 
+  Note that the answer must be a substring, "pwke" is a subsequence and not a substring. */
+
+const str4 = "dvadf"; 
+const expected4 = 4;
+// Explanation: "vadf"
+
+//Given a string, find the length of the longest substring without repeating characters.
+function lengthOfLongestSubString(str) {}
