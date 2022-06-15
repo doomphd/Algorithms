1bcf5f4bdf9dde885afad72cc1ffd9c80e802da0
Truman Tang <trumantang99@gmail.com>
2022-06-14 18:44:56 -0700

    two sum

---
+++ TwoSum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ const twoSum = function(nums, target) {
+    const obj = {};
+    for(let i=0; i<nums.length; i++){
+
+        if(obj[nums[i]] >=0){
+            return [ obj[nums[i] ] , i]
+        }
+        obj[target-nums[i]] = i
+
+    }
+};
